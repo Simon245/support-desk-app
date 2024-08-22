c0ec8e049afa5bbbb6009e24488c54cd076379e2
Simon Ball <simonball245@gmail.com>
2024-08-22 16:51:53 +0200

    Add get single ticket, delete and update tickets

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -3,8 +3,19 @@ const router = express.Router();
 const {} = require('../controllers/userController');
 
 const { protect } = require('../middleware/authMiddleware');
-const { createTicket, getTickets } = require('../controllers/ticketController');
+const {
+  createTicket,
+  getTickets,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+} = require('../controllers/ticketController');
 
 router.route('/').get(protect, getTickets).post(protect, createTicket);
+router
+  .route('/:id')
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
